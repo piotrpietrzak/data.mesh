58f8e0b4efa0203fa7afe4069d484072acdeb3fb
Piotr Pietrzak <piotr@pietrzak.me>
2024-06-30 19:33:46 +0200

    Initial commit - project generated with spring initializr

---
+++ src/main/java/net/approdorix/data/mesh/DataMeshApplication.java
@@ -0,0 +1,13 @@
+package net.approdorix.data.mesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/approdorix/data/mesh/DataMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package net.approdorix.data.mesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
